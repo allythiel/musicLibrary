dc7930bb95dc9b976507bd32f90e8517d6e92720
allythiel <allythiel@gmail.com>
2021-06-16 18:44:55 -0500

    Add Basic Framework for Index and App

---
+++ music_library/src/Components/app.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    state = {  }
+    render() {
+        return (
+            <h1>React App Component Here</h1>
+        )
+    }
+}
+
+export default App;
---
+++ music_library/src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './Components/app';
+import 'bootstrap/dist/css/bootstrap.css';
+
+const jsxElement = <h1>Music Library</h1>;
+console.log(jsxElement);
+
+ReactDOM.render(<App />, document.getElementById('root'));
