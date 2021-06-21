7b673f24154a6f84a44a55b9b8b35f1940007e96
allythiel <allythiel@gmail.com>
2021-06-21 18:52:07 -0500

    Remove Search Bar from Nav Bar

--- music_library/src/Components/NavigationBar/navigationBar.jsx
+++ music_library/src/Components/NavigationBar/navigationBar.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
-import Form from 'react-bootstrap/Form';
-import FormControl from 'react-bootstrap/FormControl';
-import Button from 'react-bootstrap/Button';
 import Nav from 'react-bootstrap/Nav';
 
 
@@ -13,10 +10,6 @@ function NavBar(props) {
             <Navbar.Brand href="#home">Music Library</Navbar.Brand>
             <Nav className="mr-auto">
             </Nav>
-            <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-1" /> 
-            <Button variant="outline-primary">Go!</Button>
-            </Form>
         </Navbar>
         </>
     )};
